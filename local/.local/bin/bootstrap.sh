eeced991b0b1409af9b982b591ddf4ce7d904b94
Lydien Sandanasamy <s.lydien@icloud.com>
2021-01-22 10:57:52 +0100

    Fix : gitmakeinstall merged with sucklesspatching

--- local/.local/bin/bootstrap.sh
+++ local/.local/bin/bootstrap.sh
@@ -5,13 +5,7 @@
 DOTFILESREPO="https://github.com/zedxlucian/dotfiles.git"
 AURHELPER="yay"
 REPOBRANCH="master"
-PACKAGES="/home/lydien/.local/bin/packages"
-
-# Sucklesspatch function variables
-DOTFILES="$HOME/.local/src/suckless"
-PROJECT_NAME="$(basename $(pwd))"
-DIFF_DIR="${DOTFILES}/${PROJECT_NAME}_diffs"
-OLD_DIFF_DIR="${DOTFILES}/${PROJECT_NAME}_diffs/old"
+PACKAGES="/tmp/packages"
 
 ### FUNCTIONS ###
 
@@ -46,7 +40,7 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 cd /tmp || return) ;}
 
 maininstall() { # Installs all needed programs from main repo.
-    echo "Installing programs"
+    echo "Installing $n of $total from MAIN"
 	installpkg "$1"
 	}
 
@@ -54,18 +48,41 @@ gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	repodir="/home/lydien/.local/src"; mkdir -p "$repodir"; chown -R lydien:wheel "$(dirname "$repodir")"
 	dir="$repodir/$progname"
+    diffdir=""$repodir"/suckless/"$progname"_diffs"
 	sudo -u "lydien" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "lydien" git pull --force origin master;}
 	cd "$dir" || exit 1
-	make >/dev/null 2>&1
-	make install >/dev/null 2>&1
-	cd /tmp || return ;}
+    echo "Installing $progname"
+    [ ! -d "$diffdir" ] && echo "No patches for $progname" && make >/dev/null 2>&1 && make install >/dev/null 2>&1 ||
+    for i in "$diffdir"/*.diff; do
+        name=$(echo "$i" | sed -e 's/\/.*\///' -e 's/\.diff//' -e "s/"$progname"_//")
+        sudo -u lydien git checkout master > /dev/null 2>&1 &&
+        sudo -u lydien git branch "$name" > /dev/null 2>&1 &&
+        sudo -u lydien git checkout "$name" > /dev/null 2>&1 &&
+        sudo -u lydien git apply "$i" > /dev/null 2>&1 &&
+        sudo -u lydien git add -A >/dev/null 2>&1 && sudo -u lydien git commit -m "Feature: Add $name patch" > /dev/null 2>&1
+    done
+    sudo -u lydien git checkout master >/dev/null 2>&1 &&
+    sudo -u lydien make clean >/dev/null 2>&1 &&
+    sudo -u lydien rm -f config.h >/dev/null 2>&1 && sudo -u lydien git reset --hard master >/dev/null 2>&1
+    sudo -u lydien git branch custom >/dev/null 2>&1 && sudo -u lydien git checkout custom >/dev/null 2>&1 &&
+    for branch in $(git for-each-ref --format='%(refname)' refs/heads/ | cut -d'/' -f3); do
+        if [ "$branch" != "master" ] && [ "$branch" != "custom" ];then
+            sudo -u lydien git rebase --rebase-merges "$branch" >/dev/null 2>&1
+        fi
+    done
+    make >/dev/null 2>&1 && make install >/dev/null 2>&1 &&
+    sudo -u lydien git checkout master >/dev/null 2>&1 && sudo -u lydien git branch -D custom >/dev/null 2>&1
+	}
 
 aurinstall() { \
+    echo "Installing $n of $total from AUR"
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
-	sudo -u "lydien" $AURHELPER -S --noconfirm "$1" >/dev/null 2>&1
+	sudo -u "lydien" $AURHELPER -S --noconfirm --needed "$1" >/dev/null 2>&1
 	}
 
 pipinstall() { \
+    echo "Installing $n of $total from PIP"
+	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
 	command -v pip || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 	}
@@ -79,7 +96,6 @@ zshconfig () {
 
 installationloop() { \
 	total=$(wc -l < $PACKAGES)
-    echo "Installing $n of $total"
 	aurinstalled=$(pacman -Qqm)
 	while IFS=, read -r tag program ; do
 		n=$((n+1))
@@ -101,60 +117,12 @@ stowinstall() {
 	sudo -u "lydien" stow -t "/home/lydien/" config home local >/dev/null 2>&1 || exit
 	chown -R "lydien":wheel "$dir" >/dev/null 2>&1 ;}
 
-systembeepoff() {
-	rmmod pcspkr
-	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
-
-sucklesspatch(){
-    echo "Installing suckless software"
-    [ ! -d "$DIFF_DIR" ] && echo "No patches" ; exit 1 ||
-	dir="$(ls "$DIFF_DIR")"
-    for i in "$dir"/*.diff; do
-        name=$(echo "$i" | sed -e 's/\/.*\///' -e 's/\.diff//' -e "s/${PROJECT_NAME}_//")
-        sudo -u lydien git checkout master > /dev/null 2>&1 &&
-        sudo -u lydien git branch "$name" > /dev/null 2>&1 &&
-        sudo -u lydien git checkout "$name" > /dev/null 2>&1 &&
-        sudo -u lydien git apply "$i" > /dev/null 2>&1 &&
-        sudo -u lydien git add -A && sudo -u lydien git commit -m "Feature: Add $name patch" > /dev/null 2>&1
-    done
-    printf "\n" &&
-    sudo -u lydien git checkout master &&
-    sudo -u lydien make clean &&
-    sudo -u lydien rm -f config.h && sudo -u lydien git reset --hard master
-}
-
-rebasemakeinstall() {
-    sudo -u lydien git checkout master &&
-    sudo -u lydien make clean &&
-    sudo -u lydien rm -f config.h && sudo -u lydien git reset --hard master
-    sudo -u lydien git branch custom && sudo -u lydien git checkout custom &&
-    for branch in $(git for-each-ref --format='%(refname)' refs/heads/ | cut -d'/' -f3); do
-        if [ "$branch" != "master" ] && [ "$branch" != "custom" ];then
-            sudo -u lydien git rebase --rebase-merges $branch
-        fi
-    done
-    sudo -u lydien make && make install &&
-    sudo -u lydien git checkout master && sudo -u lydien git branch -D custom
-}
-
-creatediff() {
-    sudo -u lydien rm -rf "$OLD_DIFF_DIR" > /dev/null 2>&1 && sudo -u lydien mkdir -p "$OLD_DIFF_DIR" && sudo -u lydien mkdir -p "$DIFF_DIR" && sudo -u lydien mv "$DIFF_DIR"*.diff "$OLD_DIFF_DIR" > /dev/null 2>&1 || true &&
-    sudo -u lydien git checkout master &&
-    sudo -u lydien make clean &&
-    sudo -u lydien rm -f config.h && sudo -u lydien git reset --hard master
-    for branch in $(git for-each-ref --format='%(refname)' refs/heads/ | cut -d'/' -f3); do
-        if [ "$branch" != "master" ] && [ "$branch" != "custom" ];then
-            sudo -u lydien git diff master.."$branch" > "${DIFF_DIR}/${PROJECT_NAME}_${branch}.diff"
-        fi
-    done
-}
-
 ### THE ACTUAL SCRIPT ###
 
 ### This is how everything happens in an intuitive format and order.
 
 # Check if user is root on Arch distro.
-[ $EUID -ne 0 ] || error "This script must be run as root"
+[ $EUID = 0 ] || error "This script must be run as root"
 
 # Refresh Arch keyrings.
 refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
@@ -169,6 +137,9 @@ ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 
 [ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
 
+# Setup git config
+gitconfig || error "Could not set git credentials globally"
+
 # Allow user to run sudo without password. Since AUR programs must be installed
 # in a fakeroot environment, this is required for all builds with AUR.
 newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
@@ -182,8 +153,8 @@ sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 manualinstall $AURHELPER || error "Failed to install AUR helper."
 
-# Adding spotify gpg key to the system as it won't work otherwise // WAITING FOR UPSTREAM FIX
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
+# Install the dotfiles in the user's home directory
+stowinstall "$DOTFILESREPO" "$REPOBRANCH"
 
 # The command that does all the installing. Reads the progs.csv file and
 # installs each needed program the way required. Be sure to run this only after
@@ -191,40 +162,15 @@ curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import 
 # and all build dependencies are installed.
 installationloop
 
-# Install the dotfiles in the user's home directory
-stowinstall "$DOTFILESREPO" "$REPOBRANCH"
-
-# Setup git config
-gitconfig || error "Could not set git credentials globally"
-
-# Most important command! Get rid of the beep!
-systembeepoff
-
 # Make zsh the default shell for the user.
 zshconfig
 
 # Use french layout globally and also use terminus-font for TTY.
 printf "KEYMAP=fr\nFONT=ter-122n\n" > /etc/vconsole.conf
 
-# Start/restart PulseAudio.
-killall pulseaudio; sudo -u "lydien" pulseaudio --start
-
 # This line, overwriting the `newperms` command above will allow the user to run
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
 newperms "%wheel ALL=(ALL) ALL
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/killall,/usr/bin/local/dwmc"
-
-# Install suckless software
-cd /home/lydien/.local/src/dwm/ || error "DWM is not present on the system."
-sucklesspatch
-
-
-cd /home/lydien/.local/src/st/ || error "ST is not present on the system."
-sucklesspatch
-rebasemakeinstall
-
-cd /home/lydien/.local/src/dmenu/ || error "DMENU is not present on the system."
-sucklesspatch
-rebasemakeinstall
 
 clear
